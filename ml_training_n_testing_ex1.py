c3289d510c4fc4cae3f7369823f533e9c369750c
Zorro30 <gbp301195@gmail.com>
2018-07-15 23:54:35 +0530

    Source Code

--- ml_training_n_testing_ex1.py
+++ ml_training_n_testing_ex1.py
@@ -1,8 +1,13 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 #df = dataframe
@@ -18,14 +23,15 @@ df.fillna(-99999, inplace=True)
 forecast_out= int(math.ceil(0.1*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 #print (df.head())
 
 X = np.array(df.drop(['label'],1)) #everything except label
-y = np.array(df['label']) # label
-
 X = preprocessing.scale(X)
-y = np.array(df['label'])
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label']) # label
 
 #print (len(X), len(y))
 
@@ -34,6 +40,31 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y ,test_
 clf = LinearRegression(n_jobs=-1) #runs as many threads available(depends on processor) to train the data.
 #clf = svm.SVR() #Switching Algorithm is simple.
 clf.fit(X_train,y_train)
+with open('linearregression.pickle','wb') as f: #we use pickle so that we dont have to train the data everytime we run the file, it just stores the trained data so that next time when we run it, it can be loaded using read.
+    pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle','rb')
+clf = pickle.load(pickle_in)
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+forecast_set = clf.predict(X_lately)
+print (forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix +=one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
