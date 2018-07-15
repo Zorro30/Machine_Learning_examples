e4d69197a4f9c416bc937ed88cb5a466c4fb71e4
Zorro30 <gbp301195@gmail.com>
2018-07-16 00:29:54 +0530

    Simple linear regression

---
+++ writing_linear_regression.py
@@ -0,0 +1,19 @@
+#writing linear regression from scratch
+
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+
+xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+ys = np.array([5,4,6,5,7,6], dtype=np.float64)
+
+def best_fit_slope(xs,ys):
+    m =( ((mean(xs) * mean(ys)) - mean(xs*ys)) / 
+        ((mean(xs)* mean(xs)) - mean(xs**2)))
+    return m
+
+m = best_fit_slope(xs,ys)
+
+print (m)
+
+
