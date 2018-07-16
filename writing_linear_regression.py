02f73b54d6a34c1a9e50b4ea62d9d12925bc469c
Zorro30 <gbp301195@gmail.com>
2018-07-17 00:24:53 +0530

    source code

--- writing_linear_regression.py
+++ writing_linear_regression.py
@@ -16,6 +16,15 @@ def best_fit_slope(xs,ys):
     b = mean(ys) - (m * mean(xs))
     return m, b
 
+def squared_error(ys_orig, ys_line): #squared error is the difference btwn orig y to the linear y.
+    return sum((ys_line - ys_orig)**2)
+
+def coefficient_of_determination(ys_orig,ys_line):
+    ys_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, ys_mean_line)
+    return 1- (squared_error_regr / squared_error_y_mean)
+
 m, b = best_fit_slope(xs,ys)
 
 regression_line = [(m*x)+b for x in xs]
@@ -23,9 +32,10 @@ regression_line = [(m*x)+b for x in xs]
 predict_x = 8
 predict_y = (m*predict_x)+b
 
+r_squared = coefficient_of_determination(ys, regression_line)
+print (r_squared)
+
 plt.scatter(xs,ys)
 plt.scatter(predict_x,predict_y, color='g')
 plt.plot(xs, regression_line)
 plt.show()
-
-
