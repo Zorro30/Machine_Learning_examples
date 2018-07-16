15d61eb17a2cbfad53c2ba8216767ac35c9fa560
Zorro30 <gbp301195@gmail.com>
2018-07-16 22:40:13 +0530

    Source Code

--- writing_linear_regression.py
+++ writing_linear_regression.py
@@ -3,6 +3,9 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
 
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,7,6], dtype=np.float64)
@@ -10,10 +13,19 @@ ys = np.array([5,4,6,5,7,6], dtype=np.float64)
 def best_fit_slope(xs,ys):
     m =( ((mean(xs) * mean(ys)) - mean(xs*ys)) / 
         ((mean(xs)* mean(xs)) - mean(xs**2)))
-    return m
+    b = mean(ys) - (m * mean(xs))
+    return m, b
 
-m = best_fit_slope(xs,ys)
+m, b = best_fit_slope(xs,ys)
 
-print (m)
+regression_line = [(m*x)+b for x in xs]
+
+predict_x = 8
+predict_y = (m*predict_x)+b
+
+plt.scatter(xs,ys)
+plt.scatter(predict_x,predict_y, color='g')
+plt.plot(xs, regression_line)
+plt.show()
 
 
