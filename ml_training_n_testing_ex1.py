8ff6a804449938d23739ee08141d79c25aa4cc93
Zorro30 <gbp301195@gmail.com>
2018-07-20 20:04:02 +0530

    Updated code

--- ml_training_n_testing_ex1.py
+++ ml_training_n_testing_ex1.py
@@ -28,7 +28,7 @@ df['label'] = df[forecast_col].shift(-forecast_out)
 X = np.array(df.drop(['label'],1)) #everything except label
 X = preprocessing.scale(X)
 X_lately = X[-forecast_out:]
-X = X[:-forecast_out:]
+X = X[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label']) # label
